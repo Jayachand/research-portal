584a0b523846338e371db978227569512335d188
Jayachand <mjayachand@gmail.com>
2015-07-02 21:04:54 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,4 +4,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^polls/', include('polls.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
